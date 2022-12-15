fc0cfb0339c0ecf02ea9e0f06422a9a3d8019d6c
Jonas Peterburs <j.peterburs@protonmail.com>
2022-12-15 20:57:58 +0000

    Use simple call to action

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,15 +1,8 @@
 <template>
   <div>
     <h3>
-      Vernetze dich mit mir auf LinkedIn und GitHub, oder über GitLab:
+      Vernetze dich mit mir:
     </h3>
-
-    <p>
-      Wenn du mit mir in Kontakt treten möchtest, kannst du mir gerne eine Nachricht schicken.
-      Ich würde mich freuen, von dir zu hören und deine Fragen oder Anliegen zu besprechen.
-    </p>
-
-    <b>Ich freue mich darauf, von dir zu hören.</b>
 
     <social-media />
   </div>
